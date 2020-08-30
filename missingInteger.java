e3d3ae31ded81a397c9eef0a8ef709d2510c3c5a
nisssaarg <nisargsd1999@gmail.com>
2020-08-30 14:17:55 +0530

    Day 6

--- missingInteger.java
+++ missingInteger.java
@@ -1,6 +1,6 @@
 /*
- * Given an array of integers, find the first missing positive integer in linear time and constant space. 
- * In other words, find the lowest positive integer that does not exist in the array.
+ * Given an array of integers, find the first missing integer in linear time and constant space. 
+ * In other words, find the lowest integer that does not exist in the array.
  * The array can contain duplicates and negative numbers as well.
  * For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
  */
@@ -17,24 +17,24 @@ public class missingInteger {
 		String[] st=s.split(",");
 		int[] arr=new int[st.length];
 		int min=99,max=-99;
-		
+
 		int sum=0;
-		
+
 		for(int i=0;i<arr.length;i++) {
 			arr[i]=Integer.parseInt(st[i]);
 			//sum=sum+arr[i];
 			max=Math.max(arr[i],max);
 			min=Math.min(arr[i],min);
 		}
-		
+
 		for(int i=min;i<=max;i++)
 			sum+=i;
-		
+
 		for(int i=0;i<arr.length;i++) {
 			sum=sum-arr[i];
 		}
-		
+
 		System.out.println(sum);
-		
+
 	}
 }
---
+++ xorLL.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+struct Node {
+    int data;
+    struct Node* npx;
+};
+struct Node* XOR (struct Node *a, struct Node *b) {
+    return (struct Node*) ((uintptr_t) (a) ^ (uintptr_t) (b));
+}
+void insert(struct Node **head_ref, int data) {
+    struct Node *new_node = (struct Node *) malloc (sizeof (struct Node) );
+    new_node->data = data;
+    new_node->npx = *head_ref;
+    if (*head_ref != NULL) {
+        (*head_ref)->npx = XOR(new_node, (*head_ref)->npx);
+    }
+    *head_ref = new_node;
+}
+void printList (struct Node *head) {
+    struct Node *curr = head;
+    struct Node *prev = NULL;
+    struct Node *next;
+
+    printf ("Following are the nodes of Linked List: \n");
+
+    while (curr != NULL) {
+        printf ("%d ", curr->data);
+        next = XOR (prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
+}
+int main(){
+    struct Node *head = NULL;
+    insert(&head, 10);
+    insert(&head, 20);
+    insert(&head, 30);
+    insert(&head, 40);
+
+    printList (head);
+
+    return (0);
+}
